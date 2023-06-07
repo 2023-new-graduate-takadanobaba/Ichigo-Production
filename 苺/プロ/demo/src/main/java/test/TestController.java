6239bc47676019d500fa81494d92c9dee255ef0d
YKYUTARO <135583611+YKYUTARO@users.noreply.github.com>
2023-06-07 16:45:59 +0900

    Update TestController.java

--- 苺/プロ/demo/src/main/java/test/TestController.java
+++ 苺/プロ/demo/src/main/java/test/TestController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    
+    @GetMapping("/test2")
+    public String test2(){
+        return "test2";
+    }  
     @GetMapping("/test")
     public String test(){
         return "test";
