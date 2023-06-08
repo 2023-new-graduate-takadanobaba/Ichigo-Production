a5c1b51143eea1c631929bf17b884feb558eba5a
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-08 16:40:51 +0900

    Update CalenderController.java

--- 苺/プロ/demo2/demo2/src/main/java/controller/CalenderController.java
+++ 苺/プロ/demo2/demo2/src/main/java/controller/CalenderController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CalenderController{
+    /**
+     * @return
+     */
     @RequestMapping(path="/form",method=RequestMethod.GET)
     public String Form(){
-            return"form"
+            return"form";
     }
 
 
