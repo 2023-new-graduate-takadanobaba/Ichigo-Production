4fbf7391a00b074f8107c18128f04e85f9d2ae89
tanaka7438 <syouta.tanaka@goodsun-hd.com>
2023-06-15 15:56:18 +0900

    Create KaimonoListBean.java

---
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/bean/KaimonoListBean.java
@@ -0,0 +1,64 @@
+package com.example.demo2.bean;
+
+
+public class KaimonoListBean {
+
+     
+	private Integer id;
+
+
+	private String goodsname;
+
+	
+	private Integer price;
+
+    
+    private Integer amount;
+
+
+	public Integer getId() {
+        return id;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getGoodsname() {
+        return goodsname;
+    }
+
+
+    public void setGoodsname(String goodsname) {
+        this.goodsname = goodsname;
+    }
+
+
+    public Integer getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+
+    
+
+
+        
+}
+    
