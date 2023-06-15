f37334c0575d0d036c2c11922aefbe2f06f5d443
tanaka7438 <syouta.tanaka@goodsun-hd.com>
2023-06-15 15:26:21 +0900

    Create RegisterForm.java

---
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/form/RegisterForm.java
@@ -0,0 +1,44 @@
+package com.example.demo2.form;
+
+
+public class RegisterForm {
+    
+	private String goodsname;
+
+	
+	private Integer price;
+
+    
+    private Integer amount;
+
+
+    public String getGoodsname() {
+        return goodsname;
+    }
+
+
+    public void setGoodsname(String goodsname) {
+        this.goodsname = goodsname;
+    }
+
+
+    public Integer getPrice() {
+        return price;
+    }
+
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+}
