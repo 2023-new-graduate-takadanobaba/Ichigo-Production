26740de7d11e3301ae5c5adb708d9fe3ac4749a0
tanaka7438 <syouta.tanaka@goodsun-hd.com>
2023-06-15 15:56:16 +0900

    Update ListController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/ListController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/ListController.java
@@ -1,13 +1,44 @@
 package com.example.demo2;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo2.bean.KaimonoListBean;
+import com.example.demo2.entity.KaimonoList;
+import com.example.demo2.form.RegisterForm;
+import com.example.demo2.repository.KaimonoListRepository;
+
+
 
 @Controller
 public class ListController{
+
+@Autowired
+KaimonoListRepository repository;
+
+
+
 @GetMapping("/list")
 public String list(){
     return "list";
 }
 
+@GetMapping("/regist")
+public String regist(RegisterForm form, Model model){
 
+KaimonoList kaimonolist = new KaimonoList();
+
+ BeanUtils.copyProperties(form, kaimonolist, "id");
+ kaimonolist=repository.save(kaimonolist);
+ KaimonoListBean itemBean = new KaimonoListBean();
+ BeanUtils.copyProperties(kaimonolist,itemBean);
+ model.addAttribute("kaimonolists", itemBean);
+
+
+
+    return "list";
+
+}
 }
