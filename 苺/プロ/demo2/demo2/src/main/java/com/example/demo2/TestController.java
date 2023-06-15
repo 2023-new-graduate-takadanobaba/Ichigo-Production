589ebf34fca22e2fbe55851ecff041e81f32f94a
sknkn <syouya.yamada@goodsun-hd.com>
2023-06-15 15:04:47 +0900

    Update TestController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/TestController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/TestController.java
@@ -1,16 +1,25 @@
 package com.example.demo2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo2.repository.KaimonoListRepository;
 
 @Controller
 public class TestController {
+
+    @Autowired
+    KaimonoListRepository repository;
+
     @GetMapping("/test2")
     public String test2(){
         return "test2";
     }  
     @GetMapping("/test")
-    public String test(){
+    public String test(Model model){
+        model.addAttribute("items",repository.findAll());
         return "test";
     }
 
