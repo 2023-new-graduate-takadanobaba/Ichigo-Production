cff571b52936b4abc4bc72dc2368456c142b6132
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-19 09:31:49 +0900

    Update ListController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/ListController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/ListController.java
@@ -1,13 +1,17 @@
 package com.example.demo2;
-import org.springframework.beans.BeanUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo2.bean.KaimonoListBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 import com.example.demo2.entity.KaimonoList;
-import com.example.demo2.form.RegisterForm;
 import com.example.demo2.repository.KaimonoListRepository;
 
 
@@ -25,20 +29,47 @@ public String list(){
     return "list";
 }
 
-@GetMapping("/regist")
-public String regist(RegisterForm form, Model model){
+// テスト用
+// @PostMapping("/regist")
+// public String regist(RegisterForm form, Model model){
 
-KaimonoList kaimonolist = new KaimonoList();
+// KaimonoList kaimonolist = new KaimonoList();
 
- BeanUtils.copyProperties(form, kaimonolist, "id");
- kaimonolist=repository.save(kaimonolist);
- KaimonoListBean itemBean = new KaimonoListBean();
- BeanUtils.copyProperties(kaimonolist,itemBean);
- model.addAttribute("kaimonolists", itemBean);
+// form.setId(1);
+
+//  BeanUtils.copyProperties(form, kaimonolist);
+//  kaimonolist=repository.save(kaimonolist);
+//  KaimonoListBean itemBean = new KaimonoListBean();
+//  BeanUtils.copyProperties(kaimonolist,itemBean);
+//  model.addAttribute("kaimonolists", itemBean);
 
 
+
+//     return "list";
+
+// }
+
+@PostMapping("/regist")
+public String regist(@RequestParam("goodsname") List<String> goodsNames,
+                     @RequestParam("price") List<Integer> prices,
+                     @RequestParam("amount") List<Integer> amounts) {
+
+    List<KaimonoList> kaimonoLists = new ArrayList<>();
+
+    for (int i = 0; i < goodsNames.size(); i++) {
+        KaimonoList kaimonoList = new KaimonoList();
+        kaimonoList.setId(i + 1);
+        kaimonoList.setGoodsname(goodsNames.get(i));
+        kaimonoList.setPrice(prices.get(i));
+        kaimonoList.setAmount(amounts.get(i));
+
+        kaimonoLists.add(kaimonoList);
+
+    }
+
+    kaimonoLists = repository.saveAll(kaimonoLists);
 
     return "list";
-
 }
+
 }
