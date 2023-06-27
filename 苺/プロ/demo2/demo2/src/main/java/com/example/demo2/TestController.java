6c269f6a5600cdaef7c7f9b25abefcd74f70506a
sknkn <syouya.yamada@goodsun-hd.com>
2023-06-27 14:54:17 +0900

    Update TestController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/TestController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/TestController.java
@@ -1,10 +1,16 @@
 package com.example.demo2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo2.entity.KaimonoList;
 import com.example.demo2.repository.KaimonoListRepository;
 
 @Controller
@@ -27,4 +33,29 @@ public class TestController {
     public String calender(){
         return "calender";
     }
+
+@PostMapping("/test/regist")
+public String regist(@RequestParam("goodsname") List<String> goodsNames,
+                     @RequestParam("price") List<Integer> prices,
+                     @RequestParam("amount") List<Integer> amounts) {
+
+    List<KaimonoList> kaimonoLists = new ArrayList<>();
+
+    for (int i = 0; i < goodsNames.size(); i++) {
+        KaimonoList kaimonoList = new KaimonoList();
+        //kaimonoList.setId(i + 1);
+        kaimonoList.setGoodsname(goodsNames.get(i));
+        kaimonoList.setPrice(prices.get(i));
+        kaimonoList.setAmount(amounts.get(i));
+
+        kaimonoLists.add(kaimonoList);
+
+    }
+
+    kaimonoLists = repository.saveAll(kaimonoLists);
+
+    return "test";
+}
+
+
 }
