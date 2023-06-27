6a51e45bbb74f5019b7f7b4c3e0286330d950c33
YKYUTARO <135583611+YKYUTARO@users.noreply.github.com>
2023-06-27 09:24:49 +0900

    Update RegisterForm.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/form/RegisterForm.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/form/RegisterForm.java
@@ -2,54 +2,5 @@ package com.example.demo2.form;
 
 
 public class RegisterForm {
-    
-	private String goodsname;
-
-	
-	private Integer price;
-
-    
-    private Integer amount;
-
-    private Integer id;
-
-    public String getGoodsname() {
-        return goodsname;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-
-    public void setGoodsname(String goodsname) {
-        this.goodsname = goodsname;
-    }
-
-
-    public Integer getPrice() {
-        return price;
-    }
-
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
+	// にゃー
 }
