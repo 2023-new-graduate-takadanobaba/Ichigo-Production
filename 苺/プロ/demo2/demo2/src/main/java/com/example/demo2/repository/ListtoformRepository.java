50796be7c791d9abf0e8e7ffd0bda0a651ed36e7
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-28 12:00:03 +0900

    Update ListtoformRepository.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/repository/ListtoformRepository.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/repository/ListtoformRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo2.repository;
 
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo2.entity.Listtoform;
 
 public interface ListtoformRepository extends JpaRepository<Listtoform, Integer> {
-    List<Listtoform>findBycreateTime(Date date);   
+    List<Listtoform>findByCreateTime(Date date);   
 }
