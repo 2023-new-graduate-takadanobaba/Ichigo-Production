3bedef5789ef148803c341ca1e9ce91afa455ecc
sknkn <syouya.yamada@goodsun-hd.com>
2023-06-29 16:15:57 +0900

    Update form.js

--- 苺/プロ/demo2/demo2/src/main/resources/static/js/form.js
+++ 苺/プロ/demo2/demo2/src/main/resources/static/js/form.js
@@ -19,22 +19,25 @@ function viewChange(){
     window.onload = viewChange;
 }
 
-    function calculateSum() {
-        var table = document.getElementById("myTable");  // テーブルの要素を取得
-        var rows = table.getElementsByTagName("tr");  // 行の要素を取得
-        var sum = 0;  // 合計の初期値
-  
-        // 各行の4列目の<td>要素から値を取得し、合計を計算する
-        for (var i = 0; i < rows.length; i++) {
-          var cells = rows[i].getElementsByTagName("td");  // 列の要素を取得
-  
-          if (cells.length >= 4) {
-            var value = parseFloat(cells[3].innerText);  // 4列目のテキスト内容を数値に変換
-            if (!isNaN(value)) {
-              sum += value;  // 数値の場合は合計に加算
-            }
+function calculateSum() {
+    var table = document.getElementById("myTable");  // テーブルの要素を取得
+    var rows = table.getElementsByTagName("tr");  // 行の要素を取得
+    var sum = 0;  // 合計の初期値
+
+    // 各行の4列目の<input>要素からvalue属性の値を取得し、合計を計算する
+    for (var i = 0; i < rows.length; i++) {
+      var cells = rows[i].getElementsByTagName("td");  // 列の要素を取得
+
+      if (cells.length >= 4) {
+        var inputElement = cells[3].querySelector("input");  // 4列目の<input>要素を取得
+        if (inputElement) {
+          var value = parseFloat(inputElement.value);  // input要素のvalue属性の値を数値に変換
+          if (!isNaN(value)) {
+            sum += value;  // 数値の場合は合計に加算
           }
         }
+      }
+    }
   
         document.getElementById("result").value = sum;  // 結果を表示
       }
