124e9aa2bcd91e91867ea32e60cb94fae49155e6
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-30 15:52:19 +0900

    Update form.js

--- 苺/プロ/demo2/demo2/src/main/resources/static/js/form.js
+++ 苺/プロ/demo2/demo2/src/main/resources/static/js/form.js
@@ -40,4 +40,26 @@ function calculateSum() {
     }
   
         document.getElementById("result").value = sum;  // 結果を表示
+
+        // 指定したエレメント(input)が所属する行(tr)を取得
+      function gyo(obj) {
+          return obj.parentElement.parentElement;
+      }
+
+        function tanka(obj) {
+          return gyo(obj).querySelectorAll("input[id^=tanka]")[0].value;
+      }
+      
+      // 指定したエレメント(input)と同じ行にある数量を取得
+      function suryo(obj) {
+          return gyo(obj).querySelectorAll("input[id^=suryo]")[0].value;
+      }
+      
+      // 指定したエレメント(input)の横計(単価×数量)を再計算してから取得
+      function yokokei(obj) {
+          result = Number(tanka(obj)) * Number(suryo(obj));
+          gyo(obj).querySelectorAll("input[id^=yokokei]")[0].value = result;
+          return result;
+      }
+        
       }
