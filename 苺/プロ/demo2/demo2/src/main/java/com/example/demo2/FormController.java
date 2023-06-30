ddeb3e49c319af940058bac63bd9472142b63598
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-06-30 09:55:09 +0900

    Update FormController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/FormController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/FormController.java
@@ -1,8 +1,47 @@
 package com.example.demo2;
 
+import java.security.Provider.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo2.entity.Bought;
+import com.example.demo2.repository.BoughtRepository;
 
 @Controller
 public class FormController {
-    
+    @Autowired
+    BoughtRepository repository;
+
+    // formを表示する
+    @RequestMapping(path = "/form/{date}", method = RequestMethod.GET)
+    public String Form(@PathVariable String date, Model model) {
+
+        model.addAttribute("boughts", repository.findByCreateTimeContaining(date));
+
+        return "form";
+    }
+
+    // formのデータを削除する
+    public void delete(Integer id) {
+
+        // idを指定して、データベースからデータを削除する
+        repository.deleteById(id);
+    }
+
+    // 行のデータの削除を行う
+    @GetMapping("/form-delate")
+    public String delateDataString(Model model, Bought bought) {
+
+        // データベースのデータを削除する
+        delete(bought.getId());
+
+        // Formの一覧画面にリダイレクト
+        return "redirect:/form";
+    }
 }
