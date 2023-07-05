a60de24d598f5e4bd344770cce41f7a7fc49085d
sknkn <syouya.yamada@goodsun-hd.com>
2023-07-05 14:51:58 +0900

    Update list.js

--- 苺/プロ/demo2/demo2/src/main/resources/static/js/list.js
+++ 苺/プロ/demo2/demo2/src/main/resources/static/js/list.js
@@ -64,7 +64,7 @@ function addForm() {
 
         //KaimonoListのDBへ登録するための値
         var hiddenCell = document.createElement("input");
-        hiddenCell.type = "text";
+        hiddenCell.type = "hidden";
         hiddenCell.name = "check";
         hiddenCell.value = "0";
         hiddenCell.id = "check";
@@ -81,7 +81,7 @@ function addForm() {
 
         //id列を追加
         var idInput = document.createElement("input");
-        idInput.type = "text";
+        idInput.type = "hidden";
         idInput.name = "id";
         idInput.value = "0";
         checkCell.appendChild(idInput);
