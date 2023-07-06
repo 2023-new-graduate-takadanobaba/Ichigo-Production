b4433ea7c194cbf7ba869697bc75cec0f61bf94c
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-07-06 17:05:50 +0900

    Create IndexController.java

---
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/IndexController.java
@@ -0,0 +1,22 @@
+package com.example.demo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    } 
+
+    @PostMapping("/login")
+    public String login(String name,HttpSession session){
+        session.setAttribute("user",name);
+        return "redirect:/";
+    }
+}
