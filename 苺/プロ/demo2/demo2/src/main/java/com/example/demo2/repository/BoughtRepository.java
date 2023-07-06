daaf719b9ce33202e4593147ab5b0312df0cc116
tanaka7438 <syouta.tanaka@goodsun-hd.com>
2023-07-06 17:09:01 +0900

    Update BoughtRepository.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/repository/BoughtRepository.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/repository/BoughtRepository.java
@@ -1,14 +1,11 @@
 package com.example.demo2.repository;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo2.entity.Bought;
 
 public interface BoughtRepository extends JpaRepository<Bought, Integer> {
-    List<Bought>findByCreateTime(String date);   
-    List<Bought>findByCreateTimeContaining(String date);   
-
+    List<Bought> findByCreateTime(String date);   
+    List<Bought> findByCreateTimeContainingAndUser(String date, String user);   
+    List<Bought> findByUser(String user);
     
 }
