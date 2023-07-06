51a09e6973ebb5df941862e47f15fcb904110df3
korosukeewhy <135583684+korosukeewhy@users.noreply.github.com>
2023-07-06 17:05:56 +0900

    Update KaimonoListRepository.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/repository/KaimonoListRepository.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/repository/KaimonoListRepository.java
@@ -1,10 +1,12 @@
 package com.example.demo2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo2.entity.KaimonoList;
 
 
 public interface KaimonoListRepository extends JpaRepository<KaimonoList, Integer>{
-    
+    List<KaimonoList> findByUser(String user);
 }
