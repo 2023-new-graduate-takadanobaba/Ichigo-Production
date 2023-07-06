7bfaa208be860a19c31caf9ec740f6fa807919cc
sknkn <syouya.yamada@goodsun-hd.com>
2023-07-06 11:58:46 +0900

    Update ListController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/ListController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/ListController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -132,5 +133,11 @@ public class ListController {
 
         // idを指定して、データベースからデータを削除する
         repository.deleteById(id);
+    }
+
+    @GetMapping("/deleteItem/{id}")
+    public String deleteItem(@PathVariable int id){
+        repository.deleteById(id);
+        return "list";
     }
 }
