2ab714b270b8d0a7511865c8118d0a1cc39efb2f
tanaka7438 <syouta.tanaka@goodsun-hd.com>
2023-07-06 17:08:53 +0900

    Update calender.js

--- 苺/プロ/demo2/demo2/src/main/resources/static/js/calender.js
+++ 苺/プロ/demo2/demo2/src/main/resources/static/js/calender.js
@@ -133,3 +133,33 @@ function isHoliday(year, month, day) {
     }
     return [false, ""];
 }
+
+$(window).on('load',function(){
+    $('.tab li:first-of-type').addClass("active");
+    $('.area:first-of-type').addClass("is-active");
+    var hashName = location.hash;
+    TabChanger(hashName);
+});
+
+function TabChanger(hashIDName){
+    if(hashIDName){
+        $('.tab li').find('a').each(function(){
+            var idName = $(this).attr('href');
+            if(idName == hashIDName){
+                var parentElm = $(this).parent();
+                $('.tab li').removeClass("active");
+                $(parentElm).addClass("active");
+                $(".area").removeClass("is-active");
+                $(hashIDName).addClass("is-active");
+            }
+        });
+    }
+}
+
+$(function() {
+    $('.tab li a').on('click',function(){
+        var idName = $(this).attr('href');
+        TabChanger (idName);
+        return false;
+    });    
+});
