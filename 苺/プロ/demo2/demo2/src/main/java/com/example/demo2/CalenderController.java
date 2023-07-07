6895935a1d0ef1f38f5a5435ed19edf631bf65e2
sknkn <syouya.yamada@goodsun-hd.com>
2023-07-07 17:38:58 +0900

    Update CalenderController.java

--- 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/CalenderController.java
+++ 苺/プロ/demo2/demo2/src/main/java/com/example/demo2/CalenderController.java
@@ -1,10 +1,22 @@
 package com.example.demo2;
 
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo2.entity.Bought;
 import com.example.demo2.form.RegisterForm;
+import com.example.demo2.repository.BoughtRepository;
+
+
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -12,14 +24,24 @@ public class CalenderController {
     /**
      * @return
      */
-   
+    @Autowired
+    BoughtRepository repository;
 
     // カレンダーを表示する
     @GetMapping("/calendar")
-    public String ichigo() {
+        public String ichigo(HttpSession session, Model model) {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-M");
+        String str = sdf.format(now);
+        List<Bought> boughts = new ArrayList<>();
+        boughts = repository.findByCreateTimeContainingAndUser(str, ((String) session.getAttribute("user")));
+        int sum = 0;
+        for (Bought bought : boughts) {
+            sum += bought.getTotal();
+        }
+        model.addAttribute("sum", sum);
         return "calender";
     }
-
     // formの完了押したときに動く機能
     @PostMapping("/")
     public String doRegister(RegisterForm form, HttpSession session) {
